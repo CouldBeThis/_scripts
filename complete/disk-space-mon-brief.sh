d0bf0393ff298cd62b69c5e4703892d6916854ca
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2021-12-27 14:55:13 -0500

    disk-space-mon-brief: strip automatic units (GB and %)

--- complete/disk-space-mon-brief.sh
+++ complete/disk-space-mon-brief.sh
@@ -10,14 +10,14 @@
 # grep -v "Filesystem"		// remove line with "Filesystem" (heading)
 # awk '{ print $5 }'		// remove all but 5th column (Use%)
 # sed 's/%//g'				// remove the "%" from values
-result=`df -kh | grep -v "Filesystem" | awk '{ print $5 }' | sed 's/%//g'`
+# result=`df -kh | grep -v "Filesystem" | awk '{ print $5 }' | sed 's/%//g'`
 
 
 # df -kh					// list all filesystems with statistics
 # head -$i					// strip all but first $i ( =2 ) lines
 # tail -1					// strip all but last line
 # awk '{print $1}'			// remove all but 1st column (Use%)
-partition=`df -kh | head -$i | tail -1| awk '{print $1}'`
+# partition=`df -kh | head -$i | tail -1| awk '{print $1}'`
 
 # echo "$partition at $(hostname -f) is ${percent}% full"
 
@@ -30,10 +30,10 @@ partition=`df -kh | head -$i | tail -1| awk '{print $1}'`
 result=`df -kh | grep "gallium" | awk '{ print $5 }' | sed 's/%//g'`
 
 partition=`df -kh | grep "gallium" | awk '{ print $1 }'`
-size=`df -kh | grep "gallium" | awk '{ print $2 }'`
-usedvalue=`df -kh | grep "gallium" | awk '{ print $3 }'`
-available=`df -kh | grep "gallium" | awk '{ print $4 }'`
-usedpercent=`df -kh | grep "gallium" | awk '{ print $5 }'`
+size=`df -kh | grep "gallium" | awk '{ print $2 }' | sed 's/G//g'`
+usedvalue=`df -kh | grep "gallium" | awk '{ print $3 }' | sed 's/G//g'`
+available=`df -kh | grep "gallium" | awk '{ print $4 }' | sed 's/G//g'`
+usedpercent=`df -kh | grep "gallium" | awk '{ print $5 }' | sed 's/%//g'`
 mountpoint=`df -kh | grep "gallium" | awk '{ print $6 }'`
 
 echo "------------"
@@ -41,11 +41,11 @@ echo "------------"
 # echo "${percent}% full"
 
 echo "Partition:	$partition"
-echo "Size:			$size"
-echo "Used:			$usedvalue"
-echo "Avail			$available"
-echo "Use%			$usedpercent"
-echo "Mount point	$mountpoint"
+echo "Size:			$size	GB"
+echo "Used:			$usedvalue	GB"
+echo "Avail			$available	GB"
+echo "Use%			$usedpercent	%"
+echo "Mount point		$mountpoint"
 
 
 
