c461359a7d623b28f280aa12857cf91a6ac8f486
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2021-12-27 15:01:09 -0500

    disk-space-mon-brief: replace hard-coded string indicating the desired target drive with a variable

--- complete/disk-space-mon-brief.sh
+++ complete/disk-space-mon-brief.sh
@@ -3,12 +3,15 @@
 ## original source (heavily modified): 
 ## https://linoxide.com/check-disk-usage-is-out-of-space/
 
-partition=`df -kh | grep "gallium" | awk '{ print $1 }'`
-size=`df -kh | grep "gallium" | awk '{ print $2 }' | sed 's/G//g'`
-usedvalue=`df -kh | grep "gallium" | awk '{ print $3 }' | sed 's/G//g'`
-available=`df -kh | grep "gallium" | awk '{ print $4 }' | sed 's/G//g'`
-usedpercent=`df -kh | grep "gallium" | awk '{ print $5 }' | sed 's/%//g'`
-mountpoint=`df -kh | grep "gallium" | awk '{ print $6 }'`
+# provide a string that is unique to the target drive
+TargetDrive="gallium"
+
+partition=`df -kh | grep "$TargetDrive" | awk '{ print $1 }'`
+size=`df -kh | grep "$TargetDrive" | awk '{ print $2 }' | sed 's/G//g'`
+usedvalue=`df -kh | grep "$TargetDrive" | awk '{ print $3 }' | sed 's/G//g'`
+available=`df -kh | grep "$TargetDrive" | awk '{ print $4 }' | sed 's/G//g'`
+usedpercent=`df -kh | grep "$TargetDrive" | awk '{ print $5 }' | sed 's/%//g'`
+mountpoint=`df -kh | grep "$TargetDrive" | awk '{ print $6 }'`
 
 echo "Partition:	$partition"
 echo "Size:			$size	GB"
