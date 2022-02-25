22a62ef60bce51532c055c20eea2e0986fe4db6f
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-02-25 03:39:22 -0500

    autosave 220225-020846

--- snippets/exa-sort.sh
+++ snippets/exa-sort.sh
@@ -3,3 +3,14 @@
 exa -lg --time-style=long-iso --color=always \
   | sed -E 's/^ *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]* *[^ ]*) *(.*)$/\1\t\3\t\4\t\2\t\5\t\6/' \
   | column -t -s $'\t' -o ' '
+
+# exa --icons --git --header --long --group-directories-first --grid --no-user --no-permissions --sort=modified
+
+## source: https://gist.github.com/robin-a-meade/fcf76e74033ec84d4d0bb6eddea209b2
+
+echo .
+echo .
+echo $DateTimeStamp
+
+git add exa-sort.sh
+git commit -m "autosave $DateTimeStamp"
