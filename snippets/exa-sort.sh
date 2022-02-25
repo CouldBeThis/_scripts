50a386697b5c31eb0c9fea6f40129e4abc3a0055
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-02-25 04:28:18 -0500

    auto save 220225-034140

--- snippets/exa-sort.sh
+++ snippets/exa-sort.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-git add exa-sort.sh
-git commit -m "autosave $DateTimeStamp"
+
+##### declarations
+#   # ⏰ Date stamps
+#   # shellcheck disable=SC2034  # "appears unused. Verify it or export it."
+    # 🗺️ Metadata 
+    WorkingDir=$PWD
+    ThisScript=$(basename "$0")
+    CurrentUser=$(whoami)
+    SomeVariable="value"
+
+#ExaCommand="exa -lg --time-style=long-iso --color=always"
+# ExaCommand="exa --header --icons -l --no-permissions --no-user --time-style=long-iso --color=always"
+
+ExaCommand="exa --header --icons -l --no-permissions --no-user --time-style=default --color=always"
+
 clear
 
-exa -lg --time-style=long-iso --color=always \
+$ExaCommand \
   | sed -E 's/^ *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]* *[^ ]*) *(.*)$/\1\t\3\t\4\t\2\t\5\t\6/' \
-  | column -t -s $'\t' -o ' '
+  | column -t -s $'\t' -o '..'
 
 # exa --icons --git --header --long --group-directories-first --grid --no-user --no-permissions --sort=modified
+
+echo "-----------------------------------" | mdcat
+$ExaCommand
+
 
 ## source: https://gist.github.com/robin-a-meade/fcf76e74033ec84d4d0bb6eddea209b2
 
