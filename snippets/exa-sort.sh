2adb7b891475ed9bca66faad67c97da2cfebf6ad
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-02-25 05:09:18 -0500

    auto save 220225-050849

--- snippets/exa-sort.sh
+++ snippets/exa-sort.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 
 ##### declarations
 #   # ⏰ Date stamps
@@ -8,23 +9,27 @@
     ThisScript=$(basename "$0")
     CurrentUser=$(whoami)
     SomeVariable="value"
+	HLine="-----------------------------"
 
 #ExaCommand="exa -lg --time-style=long-iso --color=always"
 # ExaCommand="exa --header --icons -l --no-permissions --no-user --time-style=long-iso --color=always"
 
-ExaCommand="exa --header --icons -l --no-permissions --no-user --time-style=default --color=always"
+ExaCommand="exa --header --icons -l --no-permissions --no-user --time-style=long-iso --color=always"
 
-clear
+echo $HLine | mdcat
 
 $ExaCommand \
-  | sed -E 's/^ *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]* *[^ ]*) *(.*)$/\1\t\3\t\4\t\2\t\5\t\6/' \
+  | sed -E 's/^ *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]* *[^ ]*) *(.*)$/#1\1\t#2\2\t#3\3\t#4\4\t#5\5\t#6\6/' \
   | column -t -s $'\t' -o '..'
+echo $HLine | mdcat
+
+$ExaCommand \
+  | sed -E 's/^ *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]*) *([^ ]* *[^ ]*) *(.*)$/#1\1\n#2\2\n#3\3\n#4\4\n#5\5\n#6\6/' \
+  | column -t -s $'\t' -o '..' | head -n 6
 
 # exa --icons --git --header --long --group-directories-first --grid --no-user --no-permissions --sort=modified
-
-echo "-----------------------------------" | mdcat
+echo $HLine | mdcat
 $ExaCommand
-
 
 ## source: https://gist.github.com/robin-a-meade/fcf76e74033ec84d4d0bb6eddea209b2
 
