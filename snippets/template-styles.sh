fe041bb75d328f7ea3e8635b3aabaa3e383ece89
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-03-08 15:15:31 -0500

    work on text styles scripting template

--- snippets/template-styles.sh
+++ snippets/template-styles.sh
@@ -55,22 +55,22 @@ StyleFGWhite=$(tput setaf 7)
 StyleAllOff=$(tput sgr0)		## reset all attributes
 
 	echo "┏━━━━━━━━━━━━━━◣"
-echo "$StyleRev ⎧NORMAL fg⎫ ▀$StyleAllOff ┗━━━━━━━━━━━━━━━━━━━━━━━━━━◣╲╲╲"
-echo "┃ ▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #0$StyleAllOff	▐╳╳╳"
-echo "┃ ▇$StyleFGRed$StyleSpacer$StyleU\$StyleFGRed$StyleUOff	is ANSI #1	▐╳╳╳	$StyleAllOff"
-echo "┃ ▇$StyleFGGreen$StyleSpacer$StyleU\$StyleFGGreen$StyleUOff	is ANSI #2	▐╳╳╳	$StyleAllOff"
-echo "┃ ▇$StyleFGYellow$StyleSpacer$StyleU\$StyleFGYellow$StyleUOff	is ANSI #3	▐╳╳╳	$StyleAllOff"
-echo "┃ ▇$StyleFGBlue$StyleSpacer$StyleU\$StyleFGBlue$StyleUOff	is ANSI #4	▐╳╳╳	$StyleAllOff"
-echo "┃ ▇$StyleFGMagenta$StyleSpacer$StyleU\$StyleFGMagenta$StyleUOff	is ANSI #5	▐╳╳╳	$StyleAllOff"
-echo "┃ ▇$StyleFGCyan$StyleSpacer$StyleU\$StyleFGCyan$StyleUOff	is ANSI #6	▐╳╳╳	$StyleAllOff"
-echo "┃ ▇$StyleFGWhite$StyleSpacer$StyleU\$StyleFGWhite$StyleUOff	is ANSI #7	▐╳╳╳	$StyleAllOff"
-echo "┃ ▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #8$StyleAllOff	▐╳╳╳"
+echo "$StyleRev ⎧NORMAL fg⎫ ▀$StyleAllOff ┗━━━━━━━━━━━━━━━━━━━━━━━━◣╲╲╲"
+echo "┃ ▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #0$StyleAllOff  ▐╳╳╳"
+echo "┃ ▇$StyleFGRed$StyleSpacer$StyleU\$StyleFGRed$StyleUOff	is ANSI #1  ▐╳╳╳  $StyleAllOff"
+echo "┃ ▇$StyleFGYellow$StyleSpacer$StyleU\$StyleFGYellow$StyleUOff	is ANSI #3  ▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGGreen$StyleSpacer$StyleU\$StyleFGGreen$StyleUOff	is ANSI #2  ▐╳╳╳  $StyleAllOff"
+echo "┃ ▇$StyleFGCyan$StyleSpacer$StyleU\$StyleFGCyan$StyleUOff	is ANSI #6  ▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGBlue$StyleSpacer$StyleU\$StyleFGBlue$StyleUOff	is ANSI #4  ▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGMagenta$StyleSpacer$StyleU\$StyleFGMagenta$StyleUOff	is ANSI #5  ▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGWhite$StyleSpacer$StyleU\$StyleFGWhite$StyleUOff	is ANSI #7  ▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #8$StyleAllOff  ▐╳╳╳"
 # echo "┖───────────────────────────────────────┚╱╱╱"
-echo "┃▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟╱╱╱"
+echo "┃▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟╱╱╱"
 #▙
 
 ## set BRIGHT foreground color
-StyleFGBlack=$(tput setaf 8)
+StyleFGBrBlack=$(tput setaf 8)
 StyleFGBrRed=$(tput setaf 9)
 StyleFGBrGreen=$(tput setaf 10)
 StyleFGBrYellow=$(tput setaf 11)
@@ -81,26 +81,28 @@ StyleFGBrWhite=$(tput setaf 15)
 StyleAllOff=$(tput sgr0)		## reset all attributes
 
 echo " "
-echo "$StyleRev ⎡BRIGHT fg⎤ $StyleAllOff ┣╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╮"
+echo "$StyleRev ⎡BRIGHT fg⎤ $StyleAllOff ┣╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╮"
 #															 ]
 echo "$StyleFGBrRed$StyleSpacer$StyleU\$StyleFGBrRed$StyleUOff	is ANSI #9	$StyleAllOff"
-echo "$StyleFGBrGreen$StyleSpacer$StyleU\$StyleFGBrGreen$StyleUOff	is ANSI #10	$StyleAllOff"
 echo "$StyleFGBrYellow$StyleSpacer$StyleU\$StyleFGBrYellow$StyleUOff	is ANSI #11	$StyleAllOff"
+echo "$StyleFGBrGreen$StyleSpacer$StyleU\$StyleFGBrGreen$StyleUOff	is ANSI #10	$StyleAllOff"
+echo "$StyleFGBrCyan$StyleSpacer$StyleU\$StyleFGBrCyan$StyleUOff	is ANSI #14	$StyleAllOff"
 echo "$StyleFGBrBlue$StyleSpacer$StyleU\$StyleFGBrBlue$StyleUOff	is ANSI #12	$StyleAllOff"
 echo "$StyleFGBrPurple$StyleSpacer$StyleU\$StyleFGBrPurple$StyleUOff	is ANSI #13	$StyleAllOff"
-echo "$StyleFGBrCyan$StyleSpacer$StyleU\$StyleFGBrCyan$StyleUOff	is ANSI #14	$StyleAllOff"
 echo "$StyleFGBrWhite$StyleSpacer$StyleU\$StyleFGBrWhite$StyleUOff	is ANSI #15	$StyleAllOff"
+echo "$StyleFGBrBlack$StyleSpacer$StyleU\$StyleFGBrBlack$StyleUOff	is ANSI #15	$StyleAllOff"
 echo "$StyleFGOff$StyleSpacer$StyleU\$StyleAllOff$StyleUOff	is tput sgr0	$StyleAllOff"
-echo " ┖──────────────────────────────┚"
+echo " ┖───────────────────────────────────┚"
 
 
 
 echo " "
 
 # echo "▁▂▃▄▅▆▇█		██▇▇▆▆▅▅▄▄▃▂▂▁▁"
-echo "$StyleRev ▁▁▂▂▃▃▄▄▅▅▆▆▇▇██▔▔BACKGROUND▔▔██▇▇▆▆▅▅▄▄▃▂▂▁▁  $StyleAllOff"
-echo "$StyleRev ▏▏▎▎▍▍▌▌▋▋▊▊▉███  ◀︎ colors ▶︎  ██▉▉▊▊▋▋▌▌▍▍▎▎▏▏ $StyleAllOff"
-echo "$StyleRev ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔  $StyleAllOff"
+echo "$StyleRev               ________________              $StyleAllOff"
+echo "$StyleRev  ▁▁▂▂▃▃▄▄▅▅▆▆▇█▔▔BACKGROUND▔▔█▇▆▆▅▅▄▄▃▂▂▁▁  $StyleAllOff"
+echo "$StyleRev  ▏▏▎▎▍▍▌▌▋▋▊▉██  ◀︎ colors ▶︎  █▉▊▊▋▋▌▌▍▍▎▎▏▏ $StyleAllOff"
+echo "$StyleRev  ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔  $StyleAllOff"
 echo " "
 ## set tab stops to create a compact but orderly presentation
 tabs -12
@@ -143,15 +145,15 @@ StyleAllOff=$(tput sgr0)		## reset all attributes
 tabs -14
 echo ""
 
-echo "$StyleRev ╰BRIGHT bg╮ $StyleAllOff ▙"
-echo "$StyleBGBrRed$StyleSpacer$StyleU\$StyleBGBrRed$StyleUOff	is ANSI #9	$StyleAllOff"
+echo "$StyleRev ╰ BRIGHT bg ╮ $StyleAllOff __________________________"
+echo "$StyleBGBrRed$StyleSpacer$StyleU\$StyleBGBrRed$StyleUOff	is ANSI #9	$StyleAllOff "
 echo "$StyleBGBrYellow$StyleSpacer$StyleU\$StyleBGBrYellow$StyleUOff	is ANSI #11	$StyleAllOff"
 echo "$StyleBGBrGreen$StyleSpacer$StyleU\$StyleBGBrGreen$StyleUOff	is ANSI #10	$StyleAllOff"
 echo "$StyleBGBrCyan$StyleSpacer$StyleU\$StyleBGBrCyan$StyleUOff	is ANSI #14	$StyleAllOff"
 echo "$StyleBGBrBlue$StyleSpacer$StyleU\$StyleBGBrBlue$StyleUOff	is ANSI #12	$StyleAllOff"
 echo "$StyleBGBrPurple$StyleSpacer$StyleU\$StyleBGBrPurple$StyleUOff	is ANSI #13	$StyleAllOff"
-echo "$StyleFGOff$StyleSpacer$StyleU\$StyleAllOff$StyleUOff	is tput sgr0	$StyleAllOff"
 echo "$StyleBGBrWhite$StyleSpacer$StyleU\$StyleBGBrWhite$StyleUOff	is ANSI #15	$StyleAllOff"
+echo "$StyleFGOff$StyleSpacer$StyleU\$StyleAllOff$StyleUOff	is tput sgr0	$StyleAllOff"
 echo "$StyleRev$StyleU ╰───────────────────────────────────────╯$StyleUOff$StyleAllOff"
 # echo "▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞"
 echo "〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓"
