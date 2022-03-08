cff9438a1bc73523d434aca2d9bd7829f5791fc1
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-03-08 15:54:24 -0500

    work on text styles scripting template

--- snippets/template.sh
+++ snippets/template.sh
@@ -11,31 +11,8 @@ clear
 	WorkingDir=$PWD
 	ThisScript=$(basename "$0")
 	CurrentUser=$(whoami)
-	# Text styles
-	StyleB=$(tput bold)
-	StyleDim=$(tput dim)
-	StyleU=$(tput smul)
-	StyleUOff=$(tput rmul)
-	StyleRev=$(tput rev)
-	red=`tput setaf 1`
-	green=`tput setaf 2`
-	normal=$(tput sgr0)
-
-
-tput bold    # Select bold mode
-tput dim     # Select dim (half-bright) mode
-tput smul    # Enable underline mode
-tput rmul    # Disable underline mode
-tput rev     # Turn on reverse video mode
-tput smso    # Enter standout (bold) mode
-tput rmso    # Exit standout mode
 
 	SomeVariable="value"
-
-##### text styles (see declarations)
-echo "this is ${bold}bold${normal} but this isn't"
-echo "Here is the ANSI rainbow (not reliant on declarations)"
-for (( i = 30; i < 38; i++ )); do echo -e "\033[0;"$i"m Normal: (0;$i); \033[1;"$i"m Light: (1;$i)"; done
 
 ##### get user input
 
