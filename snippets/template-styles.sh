91361fb6622206077589d09e95cdc6528cec5535
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-03-08 14:44:11 -0500

    work on text styles scripting template

--- snippets/template-styles.sh
+++ snippets/template-styles.sh
@@ -27,18 +27,20 @@ clear
 	StyleAllOff=$(tput sgr0)
 
 
-
-
-
-	echo "$StyleRev ╳╳F╳O╳R╳E╳G╳R╳O╳U╳N╳D╳╳╳╳╳x╳x╳x╳x╳x╳X╳X╳X╳X╳☓╳☓╳☓╳☓╳☓╳-╳$StyleAllOff"
-echo "$StyleRev ╳╳ ╳╳c╳o╳l╳o╳u╳r╳s╳ ╳ ╳ ╳ ╳☓╳☓╳☓╳☓╳╳☓☓☓☓$StyleAllOff"
-
-echo "FOREGROUND COLORS"
+tabs -12
+echo " $StyleRev ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔	  $StyleAllOff"
+# echo "  …╲╲╲ …………………………………………………………………………	╱╱╱ 			 $StyleAllOff"
+# echo " $StyleRev  ▽╱╲△ ╳△╳△╳△╳△╳△╳△╳△╳△╱╲╱╲╱╲╱゠゠゠"
+echo " $StyleRev ▒ ╳╳  ╳╳╳╳ ╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳ ╳╳╳╳	 ╳╳╳ $StyleAllOff"
+echo " $StyleRev ▒ ╳╳  ╳╳ ╳╳F╳O╳R╳E╳G╳R╳O╳U╳N╳D╳ ╳╳	 ╳╳╳ $StyleAllOff"
+echo " $StyleRev ▒ ╳╳  ╳╳╳ ╳╳◆╳c╳o╳l╳o╳u╳r╳s╳◆╳ ╳╳╳  ╳╳╳ $StyleAllOff"
+echo " $StyleRev ▒ ╳╳  ╳╳╳╳ ╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳ ╳╳╳╳  ╳╳╳ $StyleAllOff"
 
 ## set tab stops to create a compact but orderly presentation
-tabs -12
+tabs -14
 ## define the flair on the left edge
-StyleSpacer="▇▓▒░"
+StyleSpacer="▇▓▒░▚"
+StyleSpacerRev="▚░▒▓"
 
 
 ## set foreground color (set a F)
@@ -52,17 +54,20 @@ StyleFGCyan=$(tput setaf 6)
 StyleFGWhite=$(tput setaf 7)
 StyleAllOff=$(tput sgr0)		## reset all attributes
 
-	echo "┏━━━━━━━━━"
-echo "$StyleRev ⎧NORMAL fg⎫ $StyleAllOff ━━━━━━━━━━━━━━━━━━━━━━┓"
-echo "▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #0	$StyleAllOff"
-echo "▇$StyleFGRed$StyleSpacer$StyleU\$StyleFGRed$StyleUOff	is ANSI #1	$StyleAllOff"
-echo "▇$StyleFGGreen$StyleSpacer$StyleU\$StyleFGGreen$StyleUOff	is ANSI #2	$StyleAllOff"
-echo "▇$StyleFGYellow$StyleSpacer$StyleU\$StyleFGYellow$StyleUOff	is ANSI #3	$StyleAllOff"
-echo "▇$StyleFGBlue$StyleSpacer$StyleU\$StyleFGBlue$StyleUOff	is ANSI #4	$StyleAllOff"
-echo "▇$StyleFGMagenta$StyleSpacer$StyleU\$StyleFGMagenta$StyleUOff	is ANSI #5	$StyleAllOff"
-echo "▇$StyleFGCyan$StyleSpacer$StyleU\$StyleFGCyan$StyleUOff	is ANSI #6	$StyleAllOff"
-echo "▇$StyleFGWhite$StyleSpacer$StyleU\$StyleFGWhite$StyleUOff	is ANSI #7	$StyleAllOff"
-echo "▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #8	$StyleAllOff"
+	echo "┏━━━━━━━━━━━━━━┓"
+echo "$StyleRev ⎧NORMAL fg⎫ ▀$StyleAllOff ┗━━━━━━━━━━━━━━━━━━━━━━━━━━┓╲╲╲"
+echo "┃ ▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #0$StyleAllOff	▐╳╳╳"
+echo "┃ ▇$StyleFGRed$StyleSpacer$StyleU\$StyleFGRed$StyleUOff	is ANSI #1	▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGGreen$StyleSpacer$StyleU\$StyleFGGreen$StyleUOff	is ANSI #2	▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGYellow$StyleSpacer$StyleU\$StyleFGYellow$StyleUOff	is ANSI #3	▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGBlue$StyleSpacer$StyleU\$StyleFGBlue$StyleUOff	is ANSI #4	▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGMagenta$StyleSpacer$StyleU\$StyleFGMagenta$StyleUOff	is ANSI #5	▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGCyan$StyleSpacer$StyleU\$StyleFGCyan$StyleUOff	is ANSI #6	▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGWhite$StyleSpacer$StyleU\$StyleFGWhite$StyleUOff	is ANSI #7	▐╳╳╳	$StyleAllOff"
+echo "┃ ▇$StyleFGBlack$StyleSpacer$StyleU\$StyleFGBlack$StyleUOff	is ANSI #8$StyleAllOff	▐╳╳╳"
+# echo "┖───────────────────────────────────────┚╱╱╱"
+echo "┃▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟╱╱╱"
+#▙
 
 ## set BRIGHT foreground color
 StyleFGBlack=$(tput setaf 8)
@@ -76,7 +81,7 @@ StyleFGBrWhite=$(tput setaf 15)
 StyleAllOff=$(tput sgr0)		## reset all attributes
 
 echo " "
-echo "$StyleRev ⎡BRIGHT fg⎤ $StyleAllOff ╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾┐"
+echo "$StyleRev ⎡BRIGHT fg⎤ $StyleAllOff ╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╮"
 #															 ]
 echo "$StyleFGBrRed$StyleSpacer$StyleU\$StyleFGBrRed$StyleUOff	is ANSI #9	$StyleAllOff"
 echo "$StyleFGBrGreen$StyleSpacer$StyleU\$StyleFGBrGreen$StyleUOff	is ANSI #10	$StyleAllOff"
@@ -86,13 +91,17 @@ echo "$StyleFGBrPurple$StyleSpacer$StyleU\$StyleFGBrPurple$StyleUOff	is ANSI #13
 echo "$StyleFGBrCyan$StyleSpacer$StyleU\$StyleFGBrCyan$StyleUOff	is ANSI #14	$StyleAllOff"
 echo "$StyleFGBrWhite$StyleSpacer$StyleU\$StyleFGBrWhite$StyleUOff	is ANSI #15	$StyleAllOff"
 echo "$StyleFGOff$StyleSpacer$StyleU\$StyleAllOff$StyleUOff	is tput sgr0	$StyleAllOff"
-echo " ┖─────────────────────────────────┚"
+echo " ┖──────────────────────────────┚"
 
 
 
 echo " "
-echo "BACKGROUND COLORS"
 
+# echo "▁▂▃▄▅▆▇█		██▇▇▆▆▅▅▄▄▃▂▂▁▁"
+echo "$StyleRev ▁▁▂▂▃▃▄▄▅▅▆▆▇▇██▔▔BACKGROUND▔▔██▇▇▆▆▅▅▄▄▃▂▂▁▁  $StyleAllOff"
+echo "$StyleRev ▏▏▎▎▍▍▌▌▋▋▊▊▉███  ◀︎ colors ▶︎  ██▉▉▊▊▋▋▌▌▍▍▎▎▏▏ $StyleAllOff"
+echo "$StyleRev ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔  $StyleAllOff"
+echo " "
 ## set tab stops to create a compact but orderly presentation
 tabs -12
 ## define the flair on the left edge
@@ -110,15 +119,15 @@ StyleBGWhite=$(tput setab 7)
 StyleBGBlack=$(tput setab 8)
 StyleBGOff=$(tput setab 9)
 
-echo "$StyleRev NORMAL bg╲$StyleAllOff"
-echo "$StyleBGRed$StyleSpacer$StyleU\$StyleBGRed$StyleUOff	is ANSI #1	$StyleAllOff"
-echo "$StyleBGGreen$StyleSpacer$StyleU\$StyleBGGreen$StyleUOff	is ANSI #2	$StyleAllOff"
-echo "$StyleBGYellow$StyleSpacer$StyleU\$StyleBGYellow$StyleUOff	is ANSI #3	$StyleAllOff"
-echo "$StyleBGBlue$StyleSpacer$StyleU\$StyleBGBlue$StyleUOff	is ANSI #4	$StyleAllOff"
-echo "$StyleBGMagenta$StyleSpacer$StyleU\$StyleBGMagenta$StyleUOff	is ANSI #5	$StyleAllOff"
-echo "$StyleBGCyan$StyleSpacer$StyleU\$StyleBGCyan$StyleUOff	is ANSI #6	$StyleAllOff"
-echo "$StyleBGWhite$StyleSpacer$StyleU\$StyleBGWhite$StyleUOff	is ANSI #7	$StyleAllOff"
-echo "$StyleBGBlack$StyleSpacer$StyleU\$StyleBGBlack$StyleUOff	is ANSI #8	$StyleAllOff"
+echo "▟$StyleRev ◤╱◢ NORMAL bg ◣╲◥ $StyleAllOff	▶︎ ▼ ▶︎ ▲ ▶︎ ▼ ▶︎ ▲"
+echo "$StyleBGRed$StyleSpacer$StyleU\$StyleBGRed$StyleUOff	is ANSI #1	▶︎ $StyleAllOff ▶︎"
+echo "$StyleBGYellow$StyleSpacer$StyleU\$StyleBGYellow$StyleUOff	is ANSI #3	▶︎ $StyleAllOff ▶︎"
+echo "$StyleBGGreen$StyleSpacer$StyleU\$StyleBGGreen$StyleUOff	is ANSI #2	▶︎ $StyleAllOff ▶︎"
+echo "$StyleBGCyan$StyleSpacer$StyleU\$StyleBGCyan$StyleUOff	is ANSI #6	▶︎ $StyleAllOff ▶︎"
+echo "$StyleBGBlue$StyleSpacer$StyleU\$StyleBGBlue$StyleUOff	is ANSI #4	▶︎ $StyleAllOff ▶︎"
+echo "$StyleBGMagenta$StyleSpacer$StyleU\$StyleBGMagenta$StyleUOff	is ANSI #5	▶︎ $StyleAllOff ▶︎"
+echo "$StyleBGBlack$StyleSpacer$StyleU\$StyleBGBlack$StyleUOff	is ANSI #8	▶︎ $StyleAllOff ▶︎"
+echo "$StyleBGWhite$StyleSpacer$StyleU\$StyleBGWhite$StyleUOff	is ANSI #7	▶︎ $StyleAllOff ▶︎"
 
 
 ## set BRIGHT background color
@@ -134,17 +143,17 @@ StyleAllOff=$(tput sgr0)		## reset all attributes
 tabs -14
 echo ""
 
-echo "$StyleRev ╰BRIGHT bg╮ $StyleAllOff"
+echo "$StyleRev ╰BRIGHT bg╮ $StyleAllOff ▙"
 echo "$StyleBGBrRed$StyleSpacer$StyleU\$StyleBGBrRed$StyleUOff	is ANSI #9	$StyleAllOff"
-echo "$StyleBGBrGreen$StyleSpacer$StyleU\$StyleBGBrGreen$StyleUOff	is ANSI #10	$StyleAllOff"
 echo "$StyleBGBrYellow$StyleSpacer$StyleU\$StyleBGBrYellow$StyleUOff	is ANSI #11	$StyleAllOff"
+echo "$StyleBGBrGreen$StyleSpacer$StyleU\$StyleBGBrGreen$StyleUOff	is ANSI #10	$StyleAllOff"
+echo "$StyleBGBrCyan$StyleSpacer$StyleU\$StyleBGBrCyan$StyleUOff	is ANSI #14	$StyleAllOff"
 echo "$StyleBGBrBlue$StyleSpacer$StyleU\$StyleBGBrBlue$StyleUOff	is ANSI #12	$StyleAllOff"
 echo "$StyleBGBrPurple$StyleSpacer$StyleU\$StyleBGBrPurple$StyleUOff	is ANSI #13	$StyleAllOff"
-echo "$StyleBGBrCyan$StyleSpacer$StyleU\$StyleBGBrCyan$StyleUOff	is ANSI #14	$StyleAllOff"
-echo "$StyleBGBrWhite$StyleSpacer$StyleU\$StyleBGBrWhite$StyleUOff	is ANSI #15	$StyleAllOff"
 echo "$StyleFGOff$StyleSpacer$StyleU\$StyleAllOff$StyleUOff	is tput sgr0	$StyleAllOff"
+echo "$StyleBGBrWhite$StyleSpacer$StyleU\$StyleBGBrWhite$StyleUOff	is ANSI #15	$StyleAllOff"
 echo "$StyleRev$StyleU ╰───────────────────────────────────────╯$StyleUOff$StyleAllOff"
-
+echo "▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞ ▞"
 
 
 ## Generate the basic rainbow
@@ -153,12 +162,12 @@ echo "$StyleRev$StyleU ╰──────────────────
 
 
 
-echo "To see the basic 8 colors (as used by setf in urxvt terminal and setaf in xterm terminal):"
-
-printf '\e[%sm▒' {30..37} 0; echo           ### foreground
-printf '\e[%sm ' {40..47} 0; echo           ### background
+# echo "To see the basic 8 colors (as used by setf in urxvt terminal and setaf in xterm terminal):"
+# printf '\e[%sm▒' {30..37} 0; echo           ### foreground
+# printf '\e[%sm ' {40..47} 0; echo           ### background
 
 # echo "To see the extended 256 colors (as used by setaf in urxvt):"
+# echo "source: https://unix.stackexchange.com/a/269085"
 #
 # color(){
 #     for c; do
@@ -174,11 +183,9 @@ printf '\e[%sm ' {40..47} 0; echo           ### background
 # done
 # color {232..255}
 # 
-echo "source: https://unix.stackexchange.com/a/269085"
 
-echo "----------------------------"
 
-echo "----------------------------"
+
 
 
 
