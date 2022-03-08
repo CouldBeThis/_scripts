12be3830851c2a6179e635c6c4dfb969e93f879e
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-03-08 06:01:55 -0500

    add requesting input from user to template

--- snippets/template.sh
+++ snippets/template.sh
@@ -15,9 +15,16 @@ clear
 	
 	SomeVariable="value"
 
-	
-	
-	
+##### get user input
+
+##### ask for a regular value
+read -p "Enter a value : "  InputValue	
+echo "you entered $InputValue"
+
+##### ask for a password
+read -sp "Enter a password (will not be shown): " UserPassword
+echo "Your password is $UserPassword"
+
 	
 	
 	
