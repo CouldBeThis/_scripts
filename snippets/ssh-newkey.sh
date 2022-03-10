dbe3133b978a7233f7a9c745747dc7994c677959
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-03-10 05:43:19 +0000

    Update ssh-newkey.sh

--- snippets/ssh-newkey.sh
+++ snippets/ssh-newkey.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-read -p "Text for Comment, such as email address (optional): "  Comment	
+###	Text styles
+	## Bold
+	StyleBold=$(tput smso)
+	StyleBoldOff=$(tput rmso)
+	## Reverse
+	StyleRev=$(tput rev)
+	## reset all attributes
+	StyleAllOff=$(tput sgr0)
+	## Set some colors
+	StyleFGBlue=$(tput setaf 4)		# text			= blue
+	StyleFGBrWhite=$(tput setaf 15)	# text 			= bright white
+	StyleBGWhite=$(tput setab 7)	# background	= white
+	StyleBGBlue=$(tput setab 4)		# background	= blue
 
-echo "key will be created in ~/.ssh/"
+
+read -p "Text for Comment, such as email address (optional): " Comment	
+
+SshDir="~/.ssh/"
+echo "key will be created in $SshDir"
 
 read -p "filename for key: " KeyName
+KeyPath=$SshDir$KeyName
 
-ssh-keygen -t ed25519 -f ~/.ssh/$KeyName -C "$Comment"
+$StyleFGBlue
+ssh-keygen -t ed25519 -f $KeyPath -C "$Comment"
+$StyleAllOff
+
+echo "public keys available on system:"
+
+ls -al $SshPath*.pub
 
 echo "Starting ssh-agent"
 eval "$(ssh-agent -s)"
 
-echo "Adding ~/.ssh/$KeyName to agent"
+echo "Adding $KeyPath to agent"
 ssh-add ~/.ssh/$Keyname
 
 
