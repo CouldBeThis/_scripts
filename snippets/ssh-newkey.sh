5263f0410ff9f27d528fd6a8e5f4f19691f6c8dc
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-03-10 05:29:00 +0000

    Update ssh-newkey.sh

--- snippets/ssh-newkey.sh
+++ snippets/ssh-newkey.sh
@@ -8,7 +8,11 @@ read -p "filename for key: " KeyName
 
 ssh-keygen -t ed25519 -f ~/.ssh/$KeyName -C "$Comment"
 
+echo "Starting ssh-agent"
+eval "$(ssh-agent -s)"
 
+echo "Adding ~/.ssh/$KeyName to agent"
+ssh-add ~/.ssh/$Keyname
 
 
 
