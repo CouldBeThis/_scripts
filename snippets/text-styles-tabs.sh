79ad786fa887990ad4c50747ffabc6f74f4ecd04
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2023-03-27 00:16:48 -0400

    file showing different tabs settings

---
+++ snippets/text-styles-tabs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+tabs -4 ##default
+	echo -e "$u\eetabs$uo:	4	▇	▓	▒	░	"
+tabs -6 ##default
+	echo -e "$u\eetabs$uo:	6	▇	▓	▒	░	"
+tabs -8 ##default
+	echo -e "$u\eetabs$uo:	8	▇	▓	▒	░	"
+tabs -10
+	echo -e "$u\eetabs$uo:	10	▇	▓	▒	░	"
+tabs -12
+	echo -e "$u\eetabs$uo:	12	▇	▓	▒	░	"
+
+echo "-----------------------------------"
+
+tabs -4 ##default
+	echo -e "$u\eetabs$uo:  4	▇	▓	▒	░	"
+tabs -6 ##default
+	echo -e "$u\eetabs$uo:  6	▇	▓	▒	░	"
+tabs -8 ##default
+	echo -e "$u\eetabs$uo:  8	▇	▓	▒	░	"
+tabs -10
+	echo -e "$u\eetabs$uo: 10	▇	▓	▒	░	"
+tabs -12
+	echo -e "$u\eetabs$uo: 12	▇	▓	▒	░	"
+
+echo "-----------------------------------"
+
+tabs -4 ##default
+	echo -e "$u\eetabs$uo:  4
+	▇	▓	▒	░	"
+tabs -6 ##default
+	echo -e "$u\eetabs$uo:  6
+	▇	▓	▒	░	"
+tabs -8 ##default
+	echo -e "$u\eetabs$uo:  8
+	▇	▓	▒	░	"
+tabs -10
+	echo -e "$u\eetabs$uo: 10
+	▇	▓	▒	░	"
+tabs -12
+	echo -e "$u\eetabs$uo: 12
+	▇	▓	▒	░	"
+tabs -8 ##default
+	echo "resetting tabs"
